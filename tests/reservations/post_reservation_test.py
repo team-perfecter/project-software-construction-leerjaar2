f1684b6d301eaee0373639f4c0493585cabf0f4a
Luuk556 <luukweeda.2001@gmail.com>
2025-10-08 14:08:54 +0200

    Refactored the get_reservation_test tests. added tests for post: authorized, not authorized and empty reservation.

--- tests/reservations/post_reservation_test.py
+++ tests/reservations/post_reservation_test.py
@@ -1,3 +1,7 @@
+from dataclasses import asdict, dataclass
+from datetime import date
+import json
+from unittest.mock import patch
 import pytest
 from fastapi.testclient import TestClient
 from ../../app import app
@@ -15,31 +19,66 @@ return an error if there is any overlap
 
 client = TestClient(app)
 
-reservations: list = []
+@dataclass
+class Reservation:
+    parking_lot_id: int
+    payment_id: int
+    user_id: int
+    license_plate: str
+    started: str
+    
 
-def insert_reservation(reservation):
+reservations: list[Reservation] = []
+
+def fake_post_reservation(reservation: Reservation) -> None:
     reservations.append(reservation)
 
-def get_fake_reservations(rid: int):
-    result = None
+def get_fake_reservations(rid: int)-> Reservation:
+    result: Reservation = None
     for res in reservations:
         if res["id"] == rid:
             result = res
     return result
 
+def create_test_token(username: str) -> str:
+    expire: date = datetime.utcnow() + timedelta(minutes=30)
+    token: str = jwt.encode({"sub": username, "exp": expire}, SECRET_KEY, algorithm=ALGORITHM)
+    return token
+
+token: str = create_test_token("test")
+valid_header: dict[str, str] = {"Authorization": f"Bearer {token}"}
+invalid_header: dict[str, str] = {"Authorization": "Bearer invalid"}
+
 '''
 Test creating a new reservation when a user is authorized.
 '''
+@patch("path.to.function.db_post_reservation", side_effect=fake_post_reservation)
+def test_create_reservation_when_authorized() -> None:
+    data: Reservation = Reservation(1, 1, 1, "aaaaa", str(date.today()))
+    response = client.post("/reservations", headers=valid_header, json=asdict(data))
+    assert response.status_code == 200
+    assert len(reservations) == 1
+    assert reservations[0].parking_lot_id == 1
+    reservations.clear()
 
 
 '''
 Test creating a new reservation when a user is not authorized.
 '''
+@patch("path.to.function.db_post_reservation", side_effect=fake_post_reservation)
+def test_create_reservation_when_not_authorized() -> None:
+    data: Reservation = Reservation(1, 1, 1, "aaaa", str(date.today()))
+    response = client.post("/reservations", headers=invalid_header, json=asdict(data))
+    assert response.status_code == 401
 
 
 '''
 Test creating a new reservation when a user has not filled in all data
 '''
+@patch("path.to.function.db_post_reservation", side_effect=fake_post_reservation)
+def test_create_reservation_when_date_incomplete() -> None:
+    response = client.post("/reservations", headers=invalid_header, json={"parking_lot_id": 11})
+    assert response.status_code == 422
 
 '''
 Test creating a new reservation when a parking lot does not exist
