14de6df37ea1ae03211e5307a5baa14544615cb6
Jerrel <jerreljanssen2@gmail.com>
2025-10-15 11:47:49 +0200

    gdgdsfg

--- api/routes/profile.py
+++ api/routes/profile.py
@@ -1,4 +1,4 @@
-import hashlib
+from api.auth_utils import *
 from dataclasses import dataclass
 from datetime import date
 
@@ -15,15 +15,30 @@ class User:
     phone: str
     birth_year: date
 
+class LoginData(BaseModel):
+    username: str
+    password: str
+
 
 user_list: list[User] = []
 
 @app.post("/register")
 async def register(user: User):
-    hashed_password = hashlib.md5(user.password.encode()).hexdigest()
-    user.password = hashed_password
+    user.password = hash_password(user.password)
     user_list.append(user)
     return {"message": "user created succesfully"}
+
+@app.get("/login")
+async def login(data: LoginData):
+    user = next((u for u in user_list if u.username == data.username), None)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
+    
+    if not verify_password(data.password, user.password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
+
+    access_token = create_access_token({"sub": user.username})
+    return {"access_token": access_token, "token_type": "bearer"}
 
 @app.get("/get_user/{user_id}")
 async def get_user(user_id: int):
@@ -32,4 +47,6 @@ async def get_user(user_id: int):
 @app.get("/logout")
 async def logout():
     revoke_token(token)
+
+
     
