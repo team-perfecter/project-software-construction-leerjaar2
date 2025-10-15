f25e2b1d2b95fe48197d09178c95ad1374ffe80b
Ber-kan <bhgorur@gmail.com>
2025-10-15 11:21:24 +0200

    toegevoegd - imports - voorbeeld parking lot data - extra TODOs

--- api/routes/parking-lot.py
+++ api/routes/parking-lot.py
@@ -1,7 +1,32 @@
+from dataclasses import dataclass
+from datetime import date
 
-
+from fastapi import FastAPI
 
 #TODO? moet door Stelain een ok krijgen nadat server.py gerefactored is (versie 1.0 klaar is)
+
+app = FastAPI()
+
+
+
+
+""" 
+"1": {
+        "id": "1",
+        "name": "Bedrijventerrein Almere Parkeergarage",
+        "location": "Industrial Zone",
+        "address": "Schanssingel 337, 2421 BS Almere",
+        "capacity": 335,
+        "reserved": 77,
+        "tariff": 1.9,
+        "daytariff": 11,
+        "created_at": "2020-03-25",
+        "coordinates": {
+            "lat": 52.3133,
+            "lng": 5.2234
+        }
+    }
+"""
 
 # POST
 # TODO: create parking lot (admin only)             /parking-lots
@@ -13,7 +38,7 @@
 # TODO: get parking lot by lid                      /parking-lots/{lid}
 # TODO: get all sessions lot by lid (admin only)    /parking-lots/{lid}/sessions
 # TODO: get session by session sid (admin only)     /parking-lots/{lid}/sessions/{sid}
-
+# region extra
 # TODO?: get parking lots availability               /parking-lots/availability
 # TODO?: get parking lot availability by id          /parking-lots/{id}/availability
 # TODO?: search parking lots                         /parking-lots/search
@@ -21,6 +46,7 @@
 # TODO?: get parking lots by location                /parking-lots/location/{location}
 # TODO?: get parking lot reservations                /parking-lots/{id}/reservations
 # TODO?: get parking lot stats (admin only)          /parking-lots/{id}/stats
+# endregion
 
 # PUT
 # TODO: update parking lot by lid (admin only)      /parking-lots/{lid}
