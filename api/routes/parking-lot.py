a31979b700bbe515152768e9918157282f6319ad
Ber-kan <bhgorur@gmail.com>
2025-10-15 11:36:03 +0200

    class gemaakt en db data voorbeeld

--- api/routes/parking-lot.py
+++ api/routes/parking-lot.py
@@ -7,10 +7,22 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@dataclass
+class Parkinglot:
+    id: int
+    name: str
+    location: str
+    address: str
+    capacity: int
+    reserved: int
+    tariff: float
+    daytariff: float
+    created_at: date
+    lat: float
+    lng: float
 
-
-
-""" 
+""" data
+json
 "1": {
         "id": "1",
         "name": "Bedrijventerrein Almere Parkeergarage",
@@ -26,6 +38,18 @@ app = FastAPI()
             "lng": 5.2234
         }
     }
+db
+"id": "1",
+"name": "Bedrijventerrein Almere Parkeergarage",
+"location": "Industrial Zone",
+"address": "Schanssingel 337, 2421 BS Almere",
+"capacity": 335,
+"reserved": 77,
+"tariff": 1.9,
+"daytariff": 11,
+"created_at": "2020-03-25",
+"lat": 52.3133,
+"lng": 5.2234
 """
 
 # POST
