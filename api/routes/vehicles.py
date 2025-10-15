55aa88fcd33e5c81030e70689027686237866087
NeneIsTheName <1080444@hr.nl>
2025-10-15 13:37:59 +0200

    Created get all vehicles and get vehicle of user

--- api/routes/vehicles.py
+++ api/routes/vehicles.py
@@ -1,8 +1,49 @@
 from dataclasses import dataclass
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
+
+@dataclass
+class User:
+    id: int
+    username: str
+    password: str
+    name: str
+    email: str
+    phone: str
+    role: str
+
+
+user_list: list[User] = [
+    {
+        "id": 1,
+        "username": "cindy.leenders42",
+        "password": "6b37d1ec969838d29cb611deaff50a6b",
+        "name": "Cindy Leenders",
+        "email": "cindyleenders@upcmail.nl",
+        "phone": "+310792215694",
+        "role": "USER",
+    },
+    {
+        "id": 2,
+        "username": "gijsdegraaf",
+        "password": "1b1f4e666f54b55ccd2c701ec3435dba",
+        "name": "Gijs de Graaf",
+        "email": "gijsdegraaf@hotmail.com",
+        "phone": "+310698086312",
+        "role": "ADMIN",
+    },
+    {
+        "id": 3,
+        "username": "iris.dekker70",
+        "password": "bf7ea48e511957eccb06a832ba6ae6c9",
+        "name": "Iris Dekker",
+        "email": "iris.dekker70@yahoo.com",
+        "phone": "+310207093519",
+        "role": "USER",
+    },
+]
 
 @dataclass
 class Vehicles:
@@ -15,7 +56,7 @@ class Vehicles:
     year: int
 
 
-user_list: list[Vehicles] = [
+vehicle_list: list[Vehicles] = [
     {"id": 1, "user_id": 1, "license_plate": "76-KQQ-7", "make": "Peugeot", "model": "308", "color": "Brown", "year": 2024,},
     {"id": 2, "user_id": 2, "license_plate": "45-ASP-1", "make": "Opal", "model": "308", "color": "Blue", "year": 2022,},
     {"id": 3, "user_id": 1, "license_plate": "43-ZSO-4", "make": "Peugeot", "model": "Partner", "color": "Navy", "year": 2022,},
@@ -24,15 +65,26 @@ user_list: list[Vehicles] = [
 
 #Get:
 
-#Get all vehicles from logged in user.
+temp_login_id = 2
+#Get all vehicles from logged in user or get all vehicles if loggedin is ADMIN. (User and Admin)
 @app.get("/vehicles")
 async def vehicles():
-    print("Get all vehicles.")
+    #Temperary for login.
+    auth = list(filter(lambda user: user["id"] == temp_login_id, user_list))[0]
 
-#Get vehicles of an user.
+    vehicles_user = list(filter(lambda vehicle: vehicle["user_id"] == auth["id"], vehicle_list))
+    return "No vehicles found" if vehicles_user == [] else (vehicles_user if auth["role"] == "USER" else vehicle_list)
+
+#Get one vehicle of an user. (User)
+@app.get("/vehicles/{vehicle_id}")
+async def vehicles(vehicle_id: int):
+    print("Get one vehicle of an user.")
+
+#Get vehicles of an user. (Admin)
 @app.get("/vehicles/user/{user_id}")
-async def vehicle_user(user_id):
-    print("Get vehicle of a user.")
+async def vehicle_user(user_id: int):
+    vehicles_user = list(filter(lambda vehicle: vehicle["user_id"] == user_id, vehicle_list))
+    return "Vehicles not found" if vehicles_user == [] else vehicles_user
 
 
 
