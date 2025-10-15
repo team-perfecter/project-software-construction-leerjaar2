903ce922eaf87413a3d46359af5dc0a08b9dc4f3
Ber-kan <bhgorur@gmail.com>
2025-10-15 11:07:53 +0200

    extra todos geplaatst

--- api/routes/parking-lot.py
+++ api/routes/parking-lot.py
@@ -1,5 +1,8 @@
 
 
+
+#TODO? moet door Stelain een ok krijgen nadat server.py gerefactored is (versie 1.0 klaar is)
+
 # POST
 # TODO: create parking lot (admin only)             /parking-lots
 # TODO: start parking session by lid                /parking-lots/{lid}/sessions/start
@@ -10,6 +13,14 @@
 # TODO: get parking lot by lid                      /parking-lots/{lid}
 # TODO: get all sessions lot by lid (admin only)    /parking-lots/{lid}/sessions
 # TODO: get session by session sid (admin only)     /parking-lots/{lid}/sessions/{sid}
+
+# TODO?: get parking lots availability               /parking-lots/availability
+# TODO?: get parking lot availability by id          /parking-lots/{id}/availability
+# TODO?: search parking lots                         /parking-lots/search
+# TODO?: get parking lots by city                    /parking-lots/city/{city}
+# TODO?: get parking lots by location                /parking-lots/location/{location}
+# TODO?: get parking lot reservations                /parking-lots/{id}/reservations
+# TODO?: get parking lot stats (admin only)          /parking-lots/{id}/stats
 
 # PUT
 # TODO: update parking lot by lid (admin only)      /parking-lots/{lid}
