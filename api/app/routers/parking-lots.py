b96582109ed5d469c0bb57a06494b0ddf1e3a48e
Luuk556 <luukweeda.2001@gmail.com>
2025-11-01 20:59:16 +0100

    Removed the temporary user_id in the /parking_lot/location/{location} endpoint.

--- api/app/routers/parking-lots.py
+++ api/app/routers/parking-lots.py
@@ -1,5 +1,6 @@
 from api.auth_utils import get_current_user
 from api.datatypes.parking_lot import Parking_lot
+from api.datatypes.user import User
 from api.storage.parking_lot_storage import Parking_lot_storage
 from fastapi import HTTPException, APIRouter
 import logging
@@ -16,7 +17,7 @@ logging.basicConfig(
 )
 
 @router.get("/parking_lot/{parking_lot_id}")
-async def get_parking_lot_by_id(parking_lot_id: int, current_user: str = Depends(get_current_user)):
+async def get_parking_lot_by_id(parking_lot_id: int, current_user: User = Depends(get_current_user)):
     logging.info("A user with the id of %i retrieved information about a parking lot with the id %i", 0, parking_lot_id)
     parking_lot: Parking_lot | None = storage.get_parking_lot_by_id(parking_lot_id)
     if parking_lot == None:
@@ -25,9 +26,7 @@ async def get_parking_lot_by_id(parking_lot_id: int, current_user: str = Depends
     return parking_lot
 
 @router.get("/parking_lot/location/{location}")
-async def get_parking_lot_by_id(location: str, current_user: str = Depends(get_current_user)):
-    # if not authenticated, raise httpexception
-    user_id: int = 1
+async def get_parking_lot_by_id(location: str, current_user: User = Depends(get_current_user)):
     parking_lots: list[Parking_lot] = storage.get_parking_lot_by_location(location)
-    logging.warning("A user with the ID of %i tried to retrieve parking lots in the location %s.", user_id, location)
+    logging.warning("A user with the ID of %i tried to retrieve parking lots in the location %s.", current_user.id, location)
     return parking_lots
