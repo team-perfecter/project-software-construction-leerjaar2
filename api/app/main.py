315338c313df22b8acd583fbe67be706a1b0440b
Ber-kan <bhgorur@gmail.com>
2025-11-02 19:32:19 +0100

    ontbekende imports en include router

--- api/app/main.py
+++ api/app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
-from api.app.routers import profile, reservations, parking_lot
+from api.app.routers import profile, reservations, parking_lot, session, reservations, vehicles
 
 app = FastAPI()
 
 app.include_router(profile.router)
 app.include_router(reservations.router)
+app.include_router(session.router)
 app.include_router(parking_lot.router)
+# app.include_router(vehicles.router)
