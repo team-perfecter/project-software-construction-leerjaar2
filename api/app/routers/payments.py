bd3cf97cc0234c14ac00ab75dad71ada3a318912
Jerrel <jerreljanssen2@gmail.com>
2025-11-03 11:15:12 +0100

    deleted extra code

--- api/app/routers/payments.py
+++ api/app/routers/payments.py
@@ -20,22 +20,6 @@ logging.basicConfig(
     datefmt="%Y-%m-%d %H:%M:%S"
 )
 
-@router.get("/reservations/{vehicle_id}")
-async def reservations(vehicle_id: int, current_user: User = Depends(get_current_user)):
-    vehicle: Vehicle | None = vehicle_storage.get_vehicle_by_id(vehicle_id)
-    user_id: int = current_user.id
-    if vehicle is None:
-        logging.warning("A user with the ID %i tried to retrieve a vehicle that doesnt exist: %i", user_id, vehicle_id)
-        raise HTTPException(status_code=404, detail="Vehicle not found")
-
-    if vehicle.user_id != user_id:
-        logging.warning("A user with the ID %i tried to retrieve a vehicle that doesnt belong to the user: %i", user_id, vehicle.user_id)
-        raise HTTPException(status_code=403, detail="This vehicle does not belong to the logged in user")
-
-    reservation_list: list[Reservation] = reservation_storage.get_reservations_by_vehicle_id(vehicle_id)
-
-    return reservation_list
-
 @router.get("/payments")
 async def get_payments(current_user: User = Depends(get_current_user)):
     user_id: int = current_user.id
