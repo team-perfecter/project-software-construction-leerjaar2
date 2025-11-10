848d337a3c01f129ee02a37d14e32e61bd865f47
NeneIsTheName <1080444@hr.nl>
2025-11-10 15:23:44 +0100

    changed storage to model

--- api/app/routers/vehicles.py
+++ api/app/routers/vehicles.py
@@ -2,16 +2,16 @@ from api.auth_utils import get_current_user
 from api.datatypes.user import User
 from fastapi import FastAPI, HTTPException, Body, Depends, APIRouter
 from api.storage.profile_storage import Profile_storage
-from api.storage.vehicle_storage import Vehicle_storage
+from database.model.vehicle_model import Vehicle_model
 from api.datatypes.vehicle import Vehicle
 import logging
 from starlette.responses import JSONResponse
 
 router = APIRouter(tags=["vehicles"])
 
-#Modals:
+#Models:
 users_storage: Profile_storage = Profile_storage()
-vehicle_storage: Vehicle_storage = Vehicle_storage()
+vehicle_model: Vehicle_model = Vehicle_model()
 
 
 logging.basicConfig(
@@ -33,14 +33,14 @@ auth = list(filter(lambda user: user["id"] == temp_login_id, users_storage.get_a
 @router.get("/vehicles")
 async def vehicles():
     #Get all vehicles if you are Admin or get all your owned vehicles if you are user.
-    vehicles = vehicle_storage.get_all_vehicles() if auth["role"] == "ADMIN" else vehicle_storage.get_all_user_vehicles(temp_login_id)
+    vehicles = vehicle_model.get_all_vehicles() if auth["role"] == "ADMIN" else vehicle_model.get_all_user_vehicles(auth["id"])
     return "No vehicles found" if vehicles == [] else vehicles
 
 #Get one vehicle of an user. (User and Admin)
 @router.get("/vehicles/{vehicle_id}")
 async def vehicles(vehicle_id: int):
     #Get user vehicle.
-    vehicle = vehicle_storage.get_one_vehicle(vehicle_id)
+    vehicle = vehicle_model.get_one_vehicle(vehicle_id)
 
     #Shows one vehicle if you are ADMIN or if it is the vehicle of the loggedin user.
     if auth["role"] == "ADMIN" or auth["id"] == vehicle["user_id"]:
@@ -52,7 +52,7 @@ async def vehicles(vehicle_id: int):
 @router.get("/vehicles/user/{user_id}")
 async def vehicles_user(user_id: int):
     #Get user vehicles.
-    vehicles_user = vehicle_storage.get_all_user_vehicles(user_id)
+    vehicles_user = vehicle_model.get_all_user_vehicles(user_id)
     return "Vehicles not found" if vehicles_user == [] else vehicles_user
 
 
@@ -63,7 +63,7 @@ async def vehicles_user(user_id: int):
 @router.post("/vehicles/create")
 async def vehicle_create(vehicle: dict = Body(...)):
     #Create vehicle.
-    updated_list = vehicle_storage.create_vehicle(vehicle)
+    updated_list = vehicle_model.create_vehicle(vehicle)
     return updated_list
 
 
@@ -84,7 +84,7 @@ async def vehicle_update(vehicle_id: int):
 async def vehicle_delete(vehicle_id: int, current_user: User = Depends(get_current_user)):
     user_id: int = current_user.id
 
-    vehicle: Vehicle | None = vehicle_storage.get_one_vehicle(vehicle_id)
+    vehicle: Vehicle | None = vehicle_model.get_one_vehicle(vehicle_id)
 
     if vehicle == None:
         logging.warning("A user with the ID of %i tried to delete a vehicle with the ID of %i, but the vehicle could not be found.", user_id, vehicle_id)
@@ -94,7 +94,7 @@ async def vehicle_delete(vehicle_id: int, current_user: User = Depends(get_curre
         logging.warning("A user with the ID of %i tried to delete a vehicle with the ID of %i, but the vehicle does not belong to the user.", user_id, vehicle_id)
         raise HTTPException(status_code=401, detail={"error": "Not authoized to delete this vehicle"})
     
-    vehicle_storage.delete_vehicle(vehicle.id)
+    vehicle_model.delete_vehicle(vehicle.id)
     logging.info("A user with the ID of %i succesfully deleted a vehicle with the ID of %i.", user_id, vehicle_id)
     return JSONResponse(content={"message": "Vehicle succesfully deleted"}, status_code=201)
 
