16d0ec3108054a013288f63b6d0ed06d5be51a47
luuk556 <luukweeda.2001@gmail.com>
2025-11-11 19:03:36 +0100

    test_register_new_user passed.

--- tests/auth/post_profile_test.py
+++ tests/auth/post_profile_test.py
@@ -3,7 +3,13 @@ from datetime import datetime, timedelta
 import jwt
 import pytest
 from fastapi.testclient import TestClient
-from api.main import app
+from unittest.mock import patch
+
+from api.auth_utils import SECRET_KEY
+from api.datatypes.user import UserCreate
+
+with patch("psycopg2.connect"):
+    from api.main import app
 
 client = TestClient(app)
 
@@ -11,6 +17,8 @@ client = TestClient(app)
 A function that creates a new authorization token so a user can be verified
 '''
 def create_test_token(username: str):
+    SECRET_KEY = "secret"
+    ALGORITHM = "HS256"
     expire = datetime.utcnow() + timedelta(minutes=30)
     token = jwt.encode({"sub": username, "exp": expire}, SECRET_KEY, algorithm=ALGORITHM)
     return token
@@ -21,8 +29,8 @@ valid_headers = {"Authorization": f"Bearer {valid_token}"}
 '''
 Fake data
 '''
-def fake_register_user(data):
-    if data.get("username") and data.get("password"):
+def fake_register_user(user: UserCreate):
+    if user.username and user.password and user.email:
         return {"message": "User registered successfully"}
     else:
         return {"error": "Incomplete data"}
@@ -35,16 +43,26 @@ def fake_login_user(data):
     else:
         return {"error": "User not found"}
 
+# The function this function mocks requires one input, so this function also gets one input even though it doesnt do anything
+def fake_get_empty_user(username: str):
+    return
+
 '''
 Gebruiker maakt een nieuw profiel aan.
 '''
-def test_register_new_user():
-    with patch("api.routers.profile.user_model.create_user", side_effect=fake_register_user):
-        payload = {"username": "new_user", "password": "test123"}
-        response = client.post("/register", json=payload)
-        assert response.status_code == 201
-        data = response.json()
-        assert "User created" in data["message"]
+@patch("api.app.routers.profile.user_model.create_user", side_effect=fake_register_user)
+@patch("api.app.routers.profile.user_model.get_user_by_username", side_effect=fake_get_empty_user)
+def test_register_new_user(mock_create_user, mock_get_empty_user):
+    payload = {
+        "name": "new_user",
+        "username": "new_user",
+        "password": "test123",
+        "email": "new_user@example.com"
+    }
+    response = client.post("/register", json=payload)
+    assert response.status_code == 201
+    data = response.json()
+    assert "User created" in data["message"]
 
 '''
 Probeer een profiel aan te maken met incomplete data.
