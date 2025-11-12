bf83174a1d657460e033b8221a1329b194e7bdd6
NeneIsTheName <1080444@hr.nl>
2025-11-12 12:00:44 +0100

    docker works

--- api/models/database_conn.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-
-database_conn = psycopg2.connect(
-    host="localhost",
-    port=5432,
-    database="database",
-    user="user",
-    password="password",
-)
--- api/models/vehicle_model.py
+++ api/models/vehicle_model.py
@@ -1,46 +1,59 @@
-from models.database_conn import database_conn
+
 from api.datatypes.vehicle import Vehicle
+import psycopg2
 
 #eventually the database queries / JSON write/read will be here.
 
 class Vehicle_model:
     def __init__(self):
-        self.cur = database_conn.cursor()
+        self.connection = psycopg2.connect(
+            host="db",
+            port=5432,
+            database="database",
+            user="user",
+            password="password",
+        )
 
     #Return all vehicles
     def get_all_vehicles(self):
-        self.cur.execute("SELECT * FROM vehicles")
-        return self.cur.fetchall()
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM vehicles")
+        return cursor.fetchall()
     
     #return all vehicles of user.
     def get_all_user_vehicles(self, user_id):
-        self.cur.execute("SELECT * FROM vehicles WHERE user_id = %s", (user_id))
-        return self.cur.fetchall()
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM vehicles WHERE user_id = %s", (user_id))
+        return cursor.fetchall()
     
     #Return a vehicle.
     def get_one_vehicle(self, vehicle_id):
-        self.cur.execute("SELECT * FROM vehicles WHERE id = %s", (vehicle_id))
-        return self.cur.fetchone()
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM vehicles WHERE id = %s", (vehicle_id))
+        return cursor.fetchone()
 
     #Create a vehicle.
     def create_vehicle(self, user_id, vehicle):
-        self.cur.execute("""
+        cursor = self.connection.cursor()
+        cursor.execute("""
             INSERT INTO vehicles (user_id, license_plate, make, model, color, year)
             VALUES (%s, %s, %s, %s, %s, %s)
         """, (user_id, vehicle["license_plate"], vehicle["make"], vehicle["model"], vehicle["color"], vehicle["year"]))
-        self.cur.fetchone()
+        cursor.fetchone()
         return self.get_all_user_vehicles(user_id)
 
     def update_vehicle(self, vehicle):
-        self.cur.execute("""
+        cursor = self.connection.cursor()
+        cursor.execute("""
             UPDATE vehicles
             SET license_plate=%s, make=%s, model=%s, color=%s, year=%s
             WHERE id=%s
         """, (vehicle["license_plate"], vehicle["make"], vehicle["model"], vehicle["color"], vehicle["year"], vehicle["id"]))
-        self.cur.fetchone()
+        cursor.fetchone()
 
     #Delete a vehicle.
     def delete_vehicle(self, vehicle_id):
-        self.cur.execute("DELETE FROM vehicles WHERE id=%s", (vehicle_id))
-        self.cur.fetchone()
+        cursor = self.connection.cursor()
+        cursor.execute("DELETE FROM vehicles WHERE id=%s", (vehicle_id))
+        cursor.fetchone()
         
