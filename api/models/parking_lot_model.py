55f09f853840f1fe61c29db6140fef3ec3ad44ef
Ber-kan <bhgorur@gmail.com>
2025-11-16 18:33:46 +0100

    update en delete crud enpoints vergeten

--- api/models/parking_lot_model.py
+++ api/models/parking_lot_model.py
@@ -2,6 +2,7 @@ import psycopg2
 from api.datatypes.parking_lot import Parking_lot
 from typing import List, Optional
 
+
 class ParkingLotModel:
     def __init__(self):
         self.connection = psycopg2.connect(
@@ -25,13 +26,57 @@ class ParkingLotModel:
 
     def create_parking_lot(self, lot: Parking_lot) -> None:
         cursor = self.connection.cursor()
-        cursor.execute("""
+        cursor.execute(
+            """
             INSERT INTO parking_lots 
             (name, location, address, capacity, reserved, tariff, daytariff, created_at, lat, lng)
             VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
-        """, (lot.name, lot.location, lot.address, lot.capacity, lot.reserved, 
-              lot.tariff, lot.daytariff, lot.created_at, lot.lat, lot.lng))
+        """,
+            (
+                lot.name,
+                lot.location,
+                lot.address,
+                lot.capacity,
+                lot.reserved,
+                lot.tariff,
+                lot.daytariff,
+                lot.created_at,
+                lot.lat,
+                lot.lng,
+            ),
+        )
         self.connection.commit()
+
+    def update_parking_lot(self, lot_id: int, lot: Parking_lot) -> bool:
+        cursor = self.connection.cursor()
+        cursor.execute(
+            """
+            UPDATE parking_lots 
+            SET name = %s, location = %s, address = %s, capacity = %s, 
+                reserved = %s, tariff = %s, daytariff = %s, lat = %s, lng = %s
+            WHERE id = %s;
+        """,
+            (
+                lot.name,
+                lot.location,
+                lot.address,
+                lot.capacity,
+                lot.reserved,
+                lot.tariff,
+                lot.daytariff,
+                lot.lat,
+                lot.lng,
+                lot_id,
+            ),
+        )
+        self.connection.commit()
+        return cursor.rowcount > 0
+
+    def delete_parking_lot(self, lot_id: int) -> bool:
+        cursor = self.connection.cursor()
+        cursor.execute("DELETE FROM parking_lots WHERE id = %s;", (lot_id,))
+        self.connection.commit()
+        return cursor.rowcount > 0
 
     def map_to_parking_lot(self, cursor) -> List[Parking_lot]:
         columns = [desc[0] for desc in cursor.description]
