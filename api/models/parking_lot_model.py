14626cd1adcf4e5dc189f8b5d375943d3d785286
Ber-kan <bhgorur@gmail.com>
2025-11-16 18:12:46 +0100

    tojdelijk localhost

--- api/models/parking_lot_model.py
+++ api/models/parking_lot_model.py
@@ -5,7 +5,7 @@ from typing import List, Optional
 class ParkingLotModel:
     def __init__(self):
         self.connection = psycopg2.connect(
-            host="db",
+            host="localhost",
             port=5432,
             database="database",
             user="user",
