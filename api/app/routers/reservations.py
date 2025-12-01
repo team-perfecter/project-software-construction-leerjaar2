6324e33569742e7852e419abe0c7cde5421cd189
Ber-kan <bhgorur@gmail.com>
2025-12-01 11:52:07 +0100

    aanpassinge

--- api/app/routers/reservations.py
+++ api/app/routers/reservations.py
@@ -68,15 +68,15 @@
 
 # #     # check if the vehicle and parking lot exist
 
-    parking_lot = parking_lot_storage.get_parking_lot_by_lid(parking_lot_id)
-    if parking_lot == None:
-        logging.warning("A user with the id of %i tried to create a new reservation, but the requested parking lot does not exist: %i", current_user.id, parking_lot_id)
-        raise HTTPException(status_code = 404, detail = {"message": f"Parking lot does not exist"})
+    # parking_lot = parking_lot_storage.get_parking_lot_by_lid(parking_lot_id)
+    # if parking_lot == None:
+    #     logging.warning("A user with the id of %i tried to create a new reservation, but the requested parking lot does not exist: %i", current_user.id, parking_lot_id)
+    #     raise HTTPException(status_code = 404, detail = {"message": f"Parking lot does not exist"})
     
-    vehicle: Vehicle = vehicle_storage.get_one_vehicle(vehicle_id)
-    if vehicle == None:
-        logging.warning("A user with the id of %i tried to create a new reservation, but the requested vehicle does not exist: %i", current_user.id, vehicle_id)
-        raise HTTPException(status_code = 404, detail = {"message": f"Vehicle does not exist"})
+    # vehicle: Vehicle = vehicle_storage.get_one_vehicle(vehicle_id)
+    # if vehicle == None:
+    #     logging.warning("A user with the id of %i tried to create a new reservation, but the requested vehicle does not exist: %i", current_user.id, vehicle_id)
+    #     raise HTTPException(status_code = 404, detail = {"message": f"Vehicle does not exist"})
 
 
 # #     # check if the vehicle belongs to the user
