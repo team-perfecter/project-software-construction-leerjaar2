ba232850e9a70462e378fa998e3cabfefed4f512
Jerrel <jerreljanssen2@gmail.com>
2025-12-01 23:49:09 +0100

    typo

--- api/tests/payments/post_payments_test.py
+++ api/tests/payments/post_payments_test.py
@@ -1,4 +1,3 @@
-# api/tests/payments/post_payments_test.py
 from fastapi.testclient import TestClient
 from api.main import app
 from api.auth_utils import create_access_token
@@ -6,12 +5,11 @@ from api.auth_utils import create_access_token
 client = TestClient(app)
 
 def test_create_payment_with_superadmin():
-    # Generate token for default SUPERADMIN in the DB
     token = create_access_token({"sub": "superadmin"})
     headers = {"Authorization": f"Bearer {token}"}
 
     fake_payment = {
-        "user_id": 1,       # points to the SUPERADMIN itself
+        "user_id": 1,
         "amount": 200,
         "method": "paypal"
     }
@@ -20,3 +18,4 @@ def test_create_payment_with_superadmin():
     
     assert response.status_code == 201
     assert response.json() == {"message": "Payment created successfully"}
+D
