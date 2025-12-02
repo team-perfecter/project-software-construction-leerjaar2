1842cffb4bb2ac68519b8ca0b65b53ef8f202953
Jerrel <jerreljanssen2@gmail.com>
2025-12-03 00:30:37 +0100

    pep8 for conftest en hasher.py

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -3,16 +3,18 @@ from fastapi.testclient import TestClient
 from api.main import app
 from api.auth_utils import create_access_token
 
+
 @pytest.fixture
 def client():
     """Provides a FastAPI TestClient instance."""
     return TestClient(app)
 
+
 @pytest.fixture
 def client_with_token(client):
     """
     Returns a TestClient and headers with JWT token for a given role.
-    
+
     Usage:
         client, headers = client_with_token("superadmin")
         client, headers = client_with_token("paymentadmin")
