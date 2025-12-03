d24510067196a4372cd2d88879400a7e29133f63
Jerrel <jerreljanssen2@gmail.com>
2025-12-03 12:28:23 +0100

    merge conflict fixed

--- api/main.py
+++ api/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 from api.app.routers import parking_lots, sessions, payments
 from api.app.routers import profile, reservations, vehicles
+from api.app.routers import admin_reservation_routes, admin_user_routes
 
 app = FastAPI()
 
 app.include_router(admin_user_routes.router)
 app.include_router(admin_reservation_routes.router)
-#app.include_router(reservations.router)
+app.include_router(reservations.router)
 app.include_router(profile.router)
 app.include_router(vehicles.router)
 app.include_router(reservations.router)
