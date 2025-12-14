689baa74764e7c7e6a017faae79714637bf0b463
NeneIsTheName <1080444@hr.nl>
2025-12-14 20:50:07 +0100

    delete created

---
+++ api/tests/users/test_delete_user.py
@@ -0,0 +1,21 @@
+from unittest.mock import patch
+from fastapi.testclient import TestClient
+from api.main import app
+
+client = TestClient(app)
+
+def test_delete_user_as_admin(client_with_token):
+    client, headers = client_with_token("superadmin")
+    response = client.delete("/users/5", headers=headers)
+    assert response.status_code == 200
+
+def test_delete_user_doesnt_exist(client_with_token):
+    client, headers = client_with_token("superadmin")
+    response = client.delete("/users/9999999", headers=headers)
+    assert response.status_code == 404
+
+def test_delete_user_as_user(client_with_token):
+    client, headers = client_with_token("user")
+    response = client.delete("/users/1", headers=headers)
+    assert response.status_code == 401
+
