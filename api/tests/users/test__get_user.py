4fef5b1a0e6a848a5b5815d6f458bd087f1cbb4d
NeneIsTheName <1080444@hr.nl>
2025-12-14 20:49:25 +0100

    Made different names

---
+++ api/tests/users/test__get_user.py
@@ -0,0 +1,45 @@
+from unittest.mock import patch
+from fastapi.testclient import TestClient
+from api.main import app
+
+client = TestClient(app)
+
+def test_get_all_users(client_with_token):
+    client, headers = client_with_token("superadmin")
+    response = client.get("/users", headers=headers)
+    assert response.status_code == 200
+
+
+def test_get_user_as_superadmin(client_with_token):
+    client, headers = client_with_token("superadmin")
+    response = client.get("/get_user/1", headers=headers)
+    assert response.status_code == 200
+
+
+def test_get_user_as_user(client_with_token):
+    client, headers = client_with_token("user")
+    response = client.get("/get_user/1", headers=headers)
+    assert response.status_code == 403
+
+
+def test_get_user_not_logged_in(client):
+    response = client.get("/get_user/1")
+    assert response.status_code == 401
+
+
+def test_get_user_doesnt_exist(client_with_token):
+    client, headers = client_with_token("superadmin")
+    response = client.get("/get_user/2000", headers=headers)
+    assert response.status_code == 404
+
+
+def test_profile_when_loggedin(client_with_token):
+    client, headers = client_with_token("user")
+    response = client.get("/profile", headers=headers)
+    assert response.status_code == 200
+
+
+def test_get_profile_when_not_loggedin(client):
+    response = client.get("/profile")
+    assert response.status_code == 401
+
