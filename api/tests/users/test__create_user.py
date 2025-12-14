bc3245808d3f57aaf0710f3bd7ff8cb97c9db8bd
Jerrel <jerreljanssen2@gmail.com>
2025-12-14 12:15:09 +0100

    cleaned up the test files a bit

--- api/tests/users/test__create_user.py
+++ api/tests/users/test__create_user.py
@@ -5,7 +5,6 @@ from api.main import app
 client = TestClient(app)
 
 def test_register_not_all_field_filled(client):
-    
     fake_user = {
         "password": "Waddap",
         "name": "waddap",
@@ -16,8 +15,8 @@ def test_register_not_all_field_filled(client):
     response = client.post("/register", json=fake_user)
     assert response.status_code == 422
 
+
 def test_register(client):
-    
     fake_user = {
         "username": "Waddap_User",
         "password": "Waddap",
@@ -29,8 +28,19 @@ def test_register(client):
     response = client.post("/register", json=fake_user)
     assert response.status_code == 201
 
-def test_register_same_name(client):
+
+def test_register_not_existing_password(client):
     
+    fake_user = {
+        "username": "Waddap_User",
+        "password": "woow",
+    }
+    response = client.post("/login", json=fake_user)
+    assert response.status_code == 401
+
+
+
+def test_register_same_name(client):
     fake_user = {
         "username": "Waddap_User",
         "password": "Waddap",
@@ -41,6 +51,7 @@ def test_register_same_name(client):
     }
     response = client.post("/register", json=fake_user)
     assert response.status_code == 409
+
 
 def test_create_admin(client_with_token):
     client, headers = client_with_token("superadmin")
@@ -56,6 +67,7 @@ def test_create_admin(client_with_token):
     response = client.post("/create_admin", json=fake_user, headers=headers)
     assert response.status_code == 201
 
+
 def test_create_admin_already_exists(client_with_token):
     client, headers = client_with_token("superadmin")
     fake_user = {
@@ -69,3 +81,32 @@ def test_create_admin_already_exists(client_with_token):
     }
     response = client.post("/create_admin", json=fake_user, headers=headers)
     assert response.status_code == 409
+
+
+def test_login(client): 
+    fake_user = {
+        "username": "Waddap_User",
+        "password": "Waddap",
+    }
+    response = client.post("/login", json=fake_user)
+    assert response.status_code == 200
+
+
+def test_login_not_existing_username(client):
+    fake_user = {
+        "username": "user_waddap",
+        "password": "Waddap",
+    }
+    response = client.post("/login", json=fake_user)
+    assert response.status_code == 404
+
+
+def test_logout(client_with_token):
+    client, headers = client_with_token("user")
+    response = client.post("/logout", headers=headers)
+    assert response.status_code == 200
+
+
+def test_logout_without_a_login(client):
+    response = client.post("/logout")
+    assert response.status_code == 401
--- api/tests/users/test__update_user.py
+++ api/tests/users/test__update_user.py
@@ -8,12 +8,11 @@ def test_update_profile(client):
     fake_user = {
         "username": "waddapjes",
     }
-
     response = client.post("/update_profile", json=fake_user)
     assert response.status_code == 201
 
+
 def test_update_profile_without_user_changes(client):
     fake_user = {}
-
     response = client.post("/update_profile", json=fake_user)
     assert response.status_code == 400
