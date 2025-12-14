33a10252d4e27080385ba08e485eb8a57811b4aa
NeneIsTheName <1080444@hr.nl>
2025-12-14 20:45:55 +0100

    created files

---
+++ api/tests/users/test_update_user.py
@@ -0,0 +1,22 @@
+from unittest.mock import patch
+from fastapi.testclient import TestClient
+from api.main import app
+
+client = TestClient(app)
+
+def test_update_profile(client_with_token):
+    client, headers = client_with_token("user")
+    fake_user = {
+        "username": "waddapjes",
+    }
+    response = client.put("/update_profile", json=fake_user, headers=headers)
+    assert response.status_code == 200
+
+
+def test_update_profile_without_user_changes(client_with_token):
+    client, headers = client_with_token("user")
+    fake_user = {
+        "username": "waddapjess",
+    }
+    response = client.put("/update_profile", json=fake_user, headers=headers)
+    assert response.status_code == 400
