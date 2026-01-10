51e69934d4ea570e4b6bf423f1ea6e54ee4fdea4
Jerrel <jerreljanssen2@gmail.com>
2026-01-10 22:32:44 +0100

    gitignore logs + typo fix

--- api/datatypes/user.py
+++ api/datatypes/user.py
@@ -6,7 +6,7 @@ from enum import Enum
 
 class UserRole(str, Enum):
     USER = "user"
-    LOTADMIN = "admin"
+    LOTADMIN = "lotadmin"
     PAYMENTADMIN = "paymentadmin"
     SUPERADMIN = "superadmin"
 
