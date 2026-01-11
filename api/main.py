28606f39a2dc9a475cf80d8dc3907fae9e570e0d
Jerrel <jerreljanssen2@gmail.com>
2026-01-11 13:49:03 +0100

    deleted unneccesary route

--- api/main.py
+++ api/main.py
@@ -2,11 +2,9 @@ import api.logging_config # Needs to be imported for logging to be configured.
 from fastapi import FastAPI
 from api.app.routers import parking_lots, sessions, payments
 from api.app.routers import profile, reservations, vehicles
-from api.app.routers import admin_reservation_routes
 
 app = FastAPI()
 
-app.include_router(admin_reservation_routes.router)
 app.include_router(reservations.router)
 app.include_router(profile.router)
 app.include_router(vehicles.router)
